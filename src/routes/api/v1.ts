35de9a063f4b0b237359d76f5a30e46608a230c7
Joseph Schultz <joseph@acupajoe.io>
2019-09-06 22:17:34 -0500

    Add proper routes for procreate

--- src/routes/api/v1.ts
+++ src/routes/api/v1.ts
@@ -1,16 +1,34 @@
 import * as temp from "temp";
 import * as unzip from "unzip";
+import * as yazl from "yazl";
+import * as path from "path";
 import * as fs from "fs-extra";
+import { v4 as uuidv4 } from "uuid";
+
 import { Router } from "express";
 
 import { sendError } from "../../utils";
+import Color = require("color");
 
+const Vibrant = require("node-vibrant");
 const router = Router();
+
+function runTempCleanup() {
+  return new Promise((res, rej) => {
+    temp.cleanup((err, stats) => {
+      if (err) {
+        console.error(err);
+        rej("Error cleaning temporary files");
+      }
+      res(`Cleanup: ${JSON.stringify(stats)}`);
+    });
+  });
+}
 
 // .swatches files are zipped JSON files.
 // So we have to get the file, unzip it, open
 // its contents and parse the contents (oooof)
-router.post("/parse", (req, res, next) => {
+router.post("/upload/procreate", (req, res, next) => {
   temp.track();
   req.pipe(req.busboy);
 
@@ -44,27 +62,23 @@ router.post("/parse", (req, res, next) => {
     }
   }
 
-  function handleCleanup() {
-    temp.cleanup((err, stats) => {
-      if (err) {
-        console.error("Error cleaning temporary files");
-      }
-      console.log(`Cleanup: ${JSON.stringify(stats)}`);
-    });
-  }
-
   function handleTempFile() {
     const stream = fs.createReadStream(tempFile.path);
     stream.on("data", chunk => (data += chunk.toString("utf8")));
     stream.on("end", () => {
-      handleCleanup();
-      try {
-        const parsed = JSON.parse(data);
-        res.json(parsed);
-      } catch (e) {
-        const message = "Invalid JSON in .swatches, unable to parse.";
-        return sendError(res, message, 400);
-      }
+      runTempCleanup()
+        .then(() => {
+          try {
+            const parsed = JSON.parse(data);
+            res.json(parsed);
+          } catch (e) {
+            const message = "Invalid JSON in .swatches, unable to parse.";
+            return sendError(res, message, 400);
+          }
+        })
+        .catch(err => {
+          return sendError(res, err, 500);
+        });
     });
     stream.on("error", () => {
       const message = "Unable to read temporary stream, try again later.";
@@ -73,9 +87,98 @@ router.post("/parse", (req, res, next) => {
   }
 });
 
-router.post("/export", (req, res, next) => {
-  const name = req.body.name;
-  const swatches = req.body.swatches;
+router.post("/upload/image", (req, res, next) => {
+  temp.track();
+  req.pipe(req.busboy);
+
+  // Parse Uploaded File
+  let data = "";
+  let tempFile = temp.createWriteStream();
+
+  // Register Upload Parse Handler
+  req.busboy.on("file", handleFileUpload);
+  // Register Tempfile Handler
+  tempFile.on("close", handleTempFile);
+
+  // Unzips uploaded file
+  function handleFileUpload(fieldname: string, file: NodeJS.ReadableStream) {
+    file.pipe(tempFile);
+  }
+
+  function handleTempFile() {
+    new Vibrant(tempFile.path, { colorCount: 30 }).getPalette(
+      (err, palette) => {
+        runTempCleanup()
+          .then(() => {
+            if (err) {
+              const message = "Unable to parse image. Please try again.";
+              return sendError(res, message, 500);
+            }
+            res.json(palette);
+          })
+          .catch(e => sendError(res, e, 500));
+      }
+    );
+  }
+});
+
+router.get("/export/procreate", (req, res, next) => {
+  const uuid = uuidv4();
+  const name = req.query.name;
+  const colors = JSON.parse(req.query.colors);
+  const filename = `${name}.swatches`;
+  const innerName = "Swatches.json";
+  let zipPath = null;
+  let swatchPath = null;
+
+  const data = [{ name, swatches: [] }];
+
+  for (const color of colors) {
+    const hsl = Color(color).hsl() as any;
+    const [hue, saturation, brightness] = hsl.color;
+    const alpha = hsl.valpha;
+    const colorSpace = 0;
+
+    data[0].swatches.push({
+      hue: hue / 100,
+      saturation: saturation / 100,
+      brightness: brightness / 100,
+      alpha,
+      colorSpace
+    });
+  }
+
+  function makeInnerSwatchContent(err, dirPath) {
+    const content = JSON.stringify(data, null, 2);
+    zipPath = path.join(dirPath, filename);
+    swatchPath = path.join(dirPath, innerName);
+
+    if (err) {
+      const message = `Unable to make temp files. Try again later.`;
+      return sendError(res, message, 500);
+    }
+
+    fs.writeFile(swatchPath, content, makeProcreateSwatch);
+  }
+
+  function makeProcreateSwatch(err) {
+    if (err) {
+      const message = `Unable to write inner json content. Try again later.`;
+      return sendError(res, message, 500);
+    }
+    const zip = new yazl.ZipFile();
+    zip.addFile(swatchPath, innerName);
+    zip.outputStream
+      .pipe(fs.createWriteStream(zipPath))
+      .on("close", sendDownload);
+    zip.end(zipPath);
+  }
+
+  function sendDownload() {
+    return res.download(zipPath);
+  }
+
+  temp.mkdir(uuid, makeInnerSwatchContent);
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as busboy from "connect-busboy";
+import * as cors from "cors";
+import * as helmet from "helmet";
+
 import { sendError } from "./utils";
 
 import APIV1Routes from "./routes/api/v1";
@@ -8,6 +11,11 @@ import APIV1Routes from "./routes/api/v1";
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+const corsOptions = { origin: "*" };
+
+app.use(helmet());
+app.use(cors(corsOptions));
 app.use(busboy());
 app.use(bodyParser.json());
 
